72ee9ed209776a7ec87300425db0cc3b17173e87
melika760 <72795928+melika760@users.noreply.github.com>
2024-01-26 16:59:09 +0330

    edit inputs

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,15 +1,16 @@
-import { useState } from "react";
-export default function useInput(validatevalue){
-    const[enteredvalue,setenteredvalue]=useState("");
-    const[touch,settouched]=useState(false)
-    const ValueIsvalid=validatevalue(enteredvalue);
-    const haserror=!ValueIsvalid && touch;
-    function InputchangeHandler(event){
-        setenteredvalue(event.target.value)
-    }
-    function blured(){
-        settouched(true)
-    }
-    
-return{enteredvalue,haserror,ValueIsvalid,InputchangeHandler,blured}
+import {useState} from "react";
+export default function useInput(validateValue){
+const[enteredvalue,setenteredvalue]=useState("");
+const[istouched,setistouched]=useState(false);
+const valueIsvalid=validateValue(enteredvalue);
+const hasError = !valueIsvalid && istouched;
+function Inputchangehandler(event){
+    setenteredvalue(event.target.value)
+}
+function bluredvalue(){
+    setistouched(true)
+}
+return{
+    enteredvalue,hasError,valueIsvalid,Inputchangehandler,bluredvalue
+}
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,14 +1,12 @@
 import { Fragment } from "react";
 import Header from "../Components/Header";
 import AboutUs from "../Components/AboutUs";
-import Reserve from "../Components/Reserve";
 import Footer from "../Components/Footer";
 export default function Homepage(props){
     return (
         <Fragment>
         <Header/>
         <AboutUs/>
-        <Reserve/>
         <Footer/>
         </Fragment>
           );
